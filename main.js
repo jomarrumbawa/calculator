6ce0a02864893b81bf6eb15d96ce24b690297cd4
Jomar Rumbawa <hello@jomarrumbawa.com>
2025-10-28 10:54:33 -0700

    feat: create calculator functionality

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-let firstNum = null
-let secondNum = null
+const calcDisplay = document.querySelector("#display")
+const numBtns = document.querySelectorAll("#num-btn")
+const opBtns = document.querySelectorAll("#op-btn")
+const eqlBtn = document.querySelector("#eql-btn")
+let firstNum = ''
+let secondNum = ''
 let operator = null
 
 function add(a, b) {
@@ -24,9 +28,29 @@ function operate(a, b, operator) {
       return add(a, b)
     case '-':
       return subtract(a, b)
-    case '*':
+    case '×':
       return multiply(a, b)
-    case '/':
+    case '÷':
       return divide(a, b)
   }
 }
+
+numBtns.forEach(num => num.addEventListener("click", function(event) {
+  if (firstNum.length == 0) {
+    firstNum += event.target.textContent
+    calcDisplay.textContent = firstNum
+  } else if (firstNum.length != 0 && operator != null) {
+    secondNum += event.target.textContent
+    calcDisplay.textContent = secondNum
+  }
+  
+}))
+
+opBtns.forEach(op => op.addEventListener("click", function(event) {
+  operator = event.target.textContent
+  calcDisplay.textContent = '0'
+}))
+
+eqlBtn.addEventListener("click", function(event) {
+  calcDisplay.textContent = operate(firstNum, secondNum, operator)
+})
